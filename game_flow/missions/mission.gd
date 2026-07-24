9c1b4c20ef5e8676dede4c0c79ddb52cb29e9d72
Viktorija Panovska <viktorijapanovska137@gmail.com>
2026-07-24 18:08:44 +0200

    Mission resources created and wired to mission select screen

--- game_flow/missions/mission.gd
+++
@@ -1,9 +0,0 @@
-class_name Mission extends Resource
-
-@export var _time: float
-@export var _reference_drawing: Texture2D
-
-
-func _init(time: float = 0, reference_drawing: Texture2D = null):
-    _time = time
-    _reference_drawing = reference_drawing
